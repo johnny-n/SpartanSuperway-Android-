953a55552df33f1fb57f823f41a7d9e3ccdc1805
Johnny Nguyen <johnnytn.mail@gmail.com>
2016-09-20 18:50:46 -0700

    Fixed Gradle buildscript for Anko/Kotlin. Added basic fields for SignInFragment.

--- app/src/main/java/com/engr195/spartansuperway/spartansuperway/OrderTicketFragment.java
+++
@@ -1,6 +0,0 @@
-package com.engr195.spartansuperway.spartansuperway;
-
-import android.support.v4.app.Fragment;
-
-public class OrderTicketFragment extends Fragment {
-}
--- app/src/main/java/com/engr195/spartansuperway/spartansuperway/SignUpFragment.kt
+++ app/src/main/java/com/engr195/spartansuperway/spartansuperway/SignUpFragment.kt
@@ -5,21 +5,14 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.fragment_sign_in.*
 
-class SignInFragment: Fragment() {
-
-    val parentActivity = activity as MainActivity
+class SignUpFragment: Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
-            inflater.inflate(R.layout.fragment_sign_in, container, false)
+            inflater.inflate(R.layout.fragment_sign_up, container, false)
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        signUpAccountTextView.setOnClickListener {
-            val signUpFragment = SignUpFragment()
-            parentActivity.replaceFragment(signUpFragment, false)
-        }
     }
 }
